c826dbf6789a0cdf8a45870fab37fc6ec1ea196d
Harris Mirza <mirza.harris@gmail.com>
2017-11-12 01:26:35 +0100

    Restructured

--- controller.js
+++ controller.js
@@ -10,99 +10,111 @@ var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://nyuyiemzmlswco:7fdc0f4044583567f588f8283d44b55c6755cfa81ad8ef988013859d49f83cfe@ec2-46-137-97-169.eu-west-1.compute.amazonaws.com:5432/d674efht8ffv86'; // startrek is an example database name
 var db = pgp(connectionString);
 
-function getUserDetails(req, res, next) {
-  var id = parseInt(req.params.id);
-  db.one('SELECT * FROM app_user WHERE id = $1', id)
+
+  exports.getUserDetails = function(req, res, next) {
+    console.log("User Details: " + req.body);
+    db.one('SELECT * FROM app_user WHERE id = ${id}', req.body)
     .then(function(data) {
       res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Recieved all of the user\'s details'
-        });
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Recieved all of the user\'s details'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
-function getUserDetails(req, res, next) {
-  var id = parseInt(req.params.id);
-  db.one('SELECT * FROM item WHERE id = $1', id)
+  exports.getItemDetails = function(req, res, next) {
+    console.log("Item Details: " + req.body);
+    db.one('SELECT * FROM item WHERE id = ${id}', req.body)
     .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Recieved all of the details for the item'
-        });
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Recieved all of the details for the item'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
-
-function createNewItem(req, res, next) {
-  db.none('INSERT INTO item(barcode, calories, energy, fat, saturates, carbohydrates, sugars, protein, salt)' +
+  }
+  exports.createNewItem = function(req, res, next) {
+    console.log("Create Item: " + req.body);
+    db.none('INSERT INTO item(barcode, calories, energy, fat, saturates, carbohydrates, sugars, protein, salt)' +
       'values(${barcode}, ${calories}, ${energy}, ${fat}, ${saturates}, ${carbohydrates}, ${sugars}, ${protein}, ${salt})',
-    req.body)
+      req.body)
     .then(function () {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Created an item'
-        });
+      .json({
+        status: 'success',
+        message: 'Created an item'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
-function createNewUser(req, res, next) {
-  db.none('INSERT INTO app_user(id, email, name, calories_g, energy_g, fat_g, saturates_g, carbs_g, sugars_g, protein_g, salt_g)' +
+  exports.createNewUser = function(req, res, next) {
+    console.log("Create User: " + req.body);
+    db.none('INSERT INTO app_user(id, email, name, calories_g, energy_g, fat_g, saturates_g, carbs_g, sugars_g, protein_g, salt_g)' +
       'values(${id}, ${email}, ${name}, ${calories_g}, ${energy_g}, ${fat_g}, ${saturates_g}, ${carbs_g}, ${sugars_g}, ${protein_g}, ${salt_g})',
-    req.body)
+      req.body)
     .then(function () {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Created a user'
-        });
+      .json({
+        status: 'success',
+        message: 'Created a user'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
-function consumeItem(req, res, next) {
-  db.none('INSERT INTO consumed_item(id, barcode, date)' +
+  exports.consumeItem = function(req, res, next) {
+    console.log("Consume Item: " + req.body);
+    db.none('INSERT INTO consumed_item(id, barcode, date) ' +
       'values(${id}, ${barcode}, ${date})',
-    req.body)
+      req.body)
     .then(function () {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Added to user\'s database'
-        });
+      .json({
+        status: 'success',
+        message: 'Added to user\'s database'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
-// TODO join tables
 
-// function getDailyConsumpton(req, res, next) {
-//   db.an('SELECT * FROM consumed_item()',
-//     req.body)
-//     .then(function () {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           message: 'Added to user\'s database'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
+  exports.getDailyConsumption = function(req, res, next) {
+    console.log("Daily Consumption: " + req.body);
+    let date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    let date2 = new Date();
+    date2.setHours(23);
+    date2.setMinutes(59);
+    date2.setSeconds(59);
+    db.any('SELECT SUM(calories), SUM(calories), SUM(energy), SUM(fat), SUM(saturates), SUM(carbohydrates), SUM(sugars), SUM(protein), SUM(salt) FROM ((consumed_item NATURAL JOIN item) NATURAL JOIN app_user) WHERE id = ${id} AND date < ' + date2.toISOString() + ' AND date > ' + date.toISOString() + ';'  ,
+      req.body)
+    .then(function (data) {
+      res.status(200)
+      .json({
+        status: 'success',
+        data: data
+      });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+
